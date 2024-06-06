f34f43cdc02f03c5cd5b0dde32f8373d43dfe8ef
Iman Johari <Iman.Johari@ca.ibm.com>
2024-06-05 17:42:58 -0700

    updated  config.py

--- config.py
+++ config.py
@@ -2,18 +2,16 @@
 
 
 # Replace with your IBM Cloud API key
-CLOUD_API_KEY = 'jQ9wd6iBHEUYhdVJma6iFewH0eYtauyazhsKGQWjIdu2'
+CLOUD_API_KEY = ''
 
-# In most cases, the URL for authentication should be this value.
-# If you get an authentication error, check the URL in IBM Cloud
+# IBM cloud authentication url
 AUTH_URL = 'https://iam.cloud.ibm.com/identity/token'
 
-# Make sure to provide public, text URL (not private and not streaming)
-# Prompt URL for retrieving model IDs
+# api call to get models deployed in watsonx.ai
 PROMPT_URL = 'https://us-south.ml.cloud.ibm.com/ml/v1/foundation_model_specs?version=2023-05-02&pattern=modelid_*'
 
 # URL for generating text
 GENERATE_URL = 'https://us-south.ml.cloud.ibm.com/ml/v1/text/generation?version=2023-05-29'
 
-# IBM Cloud Project ID
+# watsonx.ai Project ID
 PROJECT_ID = "c6e59745-c0ca-44a2-841b-58c857227119"
